23916db5ea0ffc32bb38473655af9951746224d0
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-14 16:00:23 +0530

    MERN React Career app write api request for Login Component using try catch block

--- src/Components/pages/FormLogin.js
+++ src/Components/pages/FormLogin.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Form.css";
+import axios from "axios";
 
 const FormLogin = () => {
+  const [email, pickEmail] = useState("");
+  const [password, pickPassword] = useState("");
+  const [loading, setLoading] = useState("");
+  const [message, updateMessage] = useState("");
+
+  const Login = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const { data } = await axios.post(
+        "http://localhost:5000/api/users/login",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      updateMessage("Logged in Successfully...");
+    } catch (error) {
+      updateMessage(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="form-content-right">
       <form className="form" noValidate>
         <h1>Login to Your Account</h1>
+        <p className="text-success">{message}</p>
         <div className="form-inputs">
           <label className="form-label">Email</label>
           <input
             className="form-input"
             type="email"
-            name="email"
             placeholder="Enter your email"
+            value={email}
+            onChange={(obj) => pickEmail(obj.target.value)}
           />
         </div>
         <div className="form-inputs">
@@ -21,11 +51,12 @@ const FormLogin = () => {
           <input
             className="form-input"
             type="password"
-            name="password"
             placeholder="Enter your password"
+            value={password}
+            onChange={(obj) => pickPassword(obj.target.value)}
           />
         </div>
-        <button className="form-input-btn" type="submit">
+        <button className="form-input-btn" onClick={Login}>
           Login
         </button>
         <span className="form-input-login">
